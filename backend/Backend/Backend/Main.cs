eaee8a290853f674dfa1a5860baa5425dd376ce4
razxr1441 <76395477+razxr-dev@users.noreply.github.com>
2021-07-24 22:09:35 +0200

    Initial commit

---
+++ backend/Backend/Backend/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AltV.Net;
+
+namespace Backend
+{
+    public class Main : Resource
+    {
+        public override void OnStart()
+        {
+            Alt.Log("Started");
+        }
+
+        public override void OnStop()
+        {
+            Alt.Log("Stopped");
+        }
+    }
+}
